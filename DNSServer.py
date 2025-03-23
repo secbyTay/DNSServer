c4846facf7e79ab745bc632beca35aacead0822f
Taylor M Smith <ts5570@nyu.edu>
2025-03-22 22:42:21 -0500

    DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -44,7 +44,7 @@ def decrypt_with_aes(encrypted_data, password, salt):
     return decrypted_data.decode('utf-8')
 
 salt = b'Tandon' # Remember it should be a byte-object
-password = 'your_email@nyu.edu'  # Replace with your NYU email address registered in Gradescope
+password = 'ts5570@nyu.edu'  # Replace with your NYU email address registered in Gradescope
 input_string = 'AlwaysWatching'
 
 encrypted_value = encrypt_with_aes(input_string, password, salt) # exfil function
@@ -89,7 +89,7 @@ dns_records = {
     },
     'nyu.edu.': {
         dns.rdatatype.A: '192.168.1.106',
-        dns.rdatatype.TXT: (encrypted_value.decode(),),
+        dns.rdatatype.TXT: (encrypted_value.decode('utf-8'),),
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.NS: 'ns1.nyu.edu.',
